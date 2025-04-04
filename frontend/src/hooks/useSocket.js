ae1ed406d0e265a9dc39f11dfa848558ed02a7cc
NofilHafeez <nofilhafeez1@gmail.com>
2025-04-04 06:14:55 +0500

    socket modefied

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
 
 // Initialize the socket
-const socket = io(import.meta.env.MODE === "development" ? "http://localhost:3000" : "/"
-);
+const socket = io();
 
 export default socket;
